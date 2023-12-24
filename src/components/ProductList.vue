ceb0441c9dbd30c376ee8a9000e6b9662908d4fe
Vid Smole <vid.smole@gmail.com>
2023-12-24 09:45:06 +0100

    Prikaži ceno

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,25 +1,18 @@
 <script>
-
 import axios from "axios";
 
 export default {
   data() {
     return {
       products: [],
-      query: ""
-    }
+      query: "",
+    };
   },
   methods: {
-    search() {
-      if (this.query) {
-        axios.get('http://35.186.240.4/data/search/' + this.query).then(response => {
-          this.products = response.data;
-        });
-      } else {
-        axios.get('http://35.186.240.4/data/products').then(response => {
-          this.products = response.data;
-        });
-      }
+    getProducts() {
+      axios.get("http://35.186.240.4/data/products").then((response) => {
+        this.products = response.data;
+      });
     },
     displayCategories(categories) {
       let categoryNames = [];
@@ -27,34 +20,40 @@ export default {
         categoryNames.push(categories[i].name);
       }
       return categoryNames.join(", ");
-    }
+    },
   },
-}
+  mounted() {
+    this.getProducts();
+  },
+};
 </script>
 
 <template>
-
-
   <div class="row row-cols-3 g-2">
     <div v-for="product in products" class="col">
       <div class="card">
         <div class="card-body">
+          <img
+            :src="product.image_url"
+            height="300"
+            width="300"
+            class="card-img-top"
+            alt="Product image" />
 
-          <img :src="product.image_url" height="300" width="300" class="card-img-top" alt="Product image">
-          
           <router-link :to="{ path: '/products/' + product.id }">
             <h5 class="card-title">{{ product.name }}</h5>
           </router-link>
+          <p class="card-text price">{{ product.prices[0].price }} €</p>
           <p class="card-text">{{ displayCategories(product.categories) }}</p>
         </div>
       </div>
-
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
-
+.price {
+  font-size: 24px;
+  text-align: center;
+}
 </style>
